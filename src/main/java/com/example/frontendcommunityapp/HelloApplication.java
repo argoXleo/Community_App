ca6e1f66caf56f7fb1aa9d578693d6da6f7106b4
Alejandro Rodriguez <alrodriguezar@unal.edu.co>
2024-07-28 23:25:55 -0500

    Addition of the createContent function

--- src/main/java/com/example/frontendcommunityapp/HelloApplication.java
+++ src/main/java/com/example/frontendcommunityapp/HelloApplication.java
@@ -3,11 +3,18 @@ package com.example.frontendcommunityapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private Pane createContent(){
+        Pane root = new Pane();
+        return root;
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
