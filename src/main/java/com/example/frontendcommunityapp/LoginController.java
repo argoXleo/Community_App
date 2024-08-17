898ef5bfd9b419315e46a453e8158871838405b6
gsilvana <162642206+gsilvana@users.noreply.github.com>
2024-08-17 18:16:32 -0500

    Login controller

---
+++ src/main/java/com/example/frontendcommunityapp/LoginController.java
@@ -0,0 +1,37 @@
+package com.example.frontendcommunityapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField userNameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    public void handleLoginButtonAction() {
+        // Logica del botón
+        String username = userNameField.getText();
+        String password = passwordField.getText();
+
+
+        if (authenticateUser(username, password)) {
+            System.out.println("Bienvenido, " + username + "!");
+        } else {
+            System.out.println("Credenciales incorrectas");
+        }
+    }
+
+    private boolean authenticateUser(String username, String password) {
+
+        return true;
+    }
+}
