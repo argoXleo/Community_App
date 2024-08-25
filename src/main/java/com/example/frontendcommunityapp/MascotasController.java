57cf5caacc50406e5e67dba97351488bae356488
gsilvana <162642206+gsilvana@users.noreply.github.com>
2024-08-24 19:55:43 -0500

    Registro Mascotas

---
+++ src/main/java/com/example/frontendcommunityapp/MascotasController.java
@@ -0,0 +1,49 @@
+package com.example.frontendcommunityapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.example.frontendcommunityapp.Model.Services.RegistroMascotas;
+
+public class MascotasController {
+    private static final Logger logger = Logger.getLogger(MascotasController.class.getName());
+
+    @FXML
+    private TextField textFieldNombreMascota;
+    @FXML
+    private TextField textFieldRaza;
+    @FXML
+    private TextField textFieldIdCasaApto;
+    @FXML
+    private CheckBox checkBoxPerdido;
+
+    @FXML
+    public void registrarMascota() {
+        try {
+            String nombre = textFieldNombreMascota.getText();
+            String raza = textFieldRaza.getText();
+            String idCasaAptoText = textFieldIdCasaApto.getText();
+
+            if (nombre.isEmpty() || raza.isEmpty() || idCasaAptoText.isEmpty()) {
+                logger.log(Level.WARNING, "Todos los campos deben ser completados.");
+                return;
+            }
+
+            int idUsuario = Integer.parseInt(idCasaAptoText);
+            boolean perdido = checkBoxPerdido.isSelected();
+
+            RegistroMascotas registro = new RegistroMascotas(nombre, raza, idUsuario, perdido);
+            registro.registrarMascota();
+
+            System.out.println("RegistroMascotas registrada: " + nombre);
+        } catch (NumberFormatException e) {
+            logger.log(Level.WARNING, "ID de usuario no válido.");
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error al registrar mascota", e);
+        }
+    }
+
+}
