4ecd6dc956196131ca7641698f7ec992297c186f
Alejandro Rodriguez <alrodriguezar@unal.edu.co>
2024-08-24 19:17:53 -0500

    Interactive Resident pagos atras and service atras

--- src/main/java/com/example/frontendcommunityapp/PagosController.java
+++ src/main/java/com/example/frontendcommunityapp/PagosController.java
@@ -51,4 +51,15 @@ public class PagosController {
     }
 
 
+    public void backToServicesResident(javafx.event.ActionEvent actionEvent) throws IOException {
+        try {
+            root = FXMLLoader.load(getClass().getResource("Services.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
